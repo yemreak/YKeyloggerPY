66a94bab81c57d1a33fb2f5c54bbe2f1ab589cb4
Yunus Emre <yedhrab@gmail.com>
2019-06-26 18:32:50 +0300

    Gereksizler kaldırıldı 🧹

--- fixer.bat
+++
@@ -1,3 +0,0 @@
-COPY %PYTHONUSERBASE%\Python37\site-packages\pywin32_system32\pywintypes37.dll C:\Windows\System32
-
-COPY %PYTHONUSERBASE%\Python37\site-packages\pywin32_system32\pywintypes37.dllpythoncom37.dll C:\Windows\System32
